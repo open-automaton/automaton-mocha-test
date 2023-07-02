b7898861313e835353cee9434c6c632a06428fba
Abbey Sparrow <abbey@khrome.net>
2023-07-02 07:13:35 -0600

    first commit

---
+++ test/test.mjs
@@ -0,0 +1,14 @@
+/* global describe:false, it:false */
+import { chai } from 'environment-safe-chai';
+import { } from '../src/index.mjs';
+const should = chai.should();
+should.exist({});
+
+describe('module', ()=>{
+    describe('performs a simple test suite', ()=>{
+        it('loads', async ()=>{
+            
+        });
+    });
+});
+
